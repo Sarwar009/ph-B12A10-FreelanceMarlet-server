ae35527e83db42f8c69d52d3de1160f2e4108c7b
Sarwar009 <ssarwar459@gmail.com>
2025-11-13 16:07:12 +0600

    accepted job route

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const cors = require ('cors');
 const dotenv = require ('dotenv');
 const {MongoClient, ServerApiVersion, ObjectId} = require ('mongodb');
 
-const port = process.env.PORT || 3000;  
-
+const port = process.env.PORT || 3000;
 
 dotenv.config ();
 const app = express ();
@@ -20,70 +19,75 @@ async function run () {
     const db = client.db ('freelanceDB');
     const jobsCollection = db.collection ('jobs');
 
-
     app.get ('/allJobs', async (req, res) => {
       const query = {};
       const jobs = await jobsCollection.find (query).toArray ();
       res.send (jobs);
     });
 
-    app.get('/allJobs/:id', async (req, res) => {
+    app.get ('/allJobs/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const job = await jobsCollection.findOne(query);
-      res.send(job);
+      const query = {_id: new ObjectId (id)};
+      const job = await jobsCollection.findOne (query);
+      res.send (job);
     });
-
-    
 
     app.post ('/allJobs', async (req, res) => {
       const newJob = req.body;
       const result = await jobsCollection.insertOne (newJob);
-        res.send (result);
+      res.send (result);
     });
-
 
     app.patch ('/updateJobs/:id', async (req, res) => {
       const id = req.params.id;
       const updatedJob = req.body;
-      
-          delete updatedJob._id;
-        const query = { _id: new ObjectId(id) };
-        const update = {
-          $set: updatedJob,
-        };
-        const result = await jobsCollection.updateOne (query, update);
-        res.send (result);
+
+      delete updatedJob._id;
+      const query = {_id: new ObjectId (id)};
+      const update = {
+        $set: updatedJob,
+      };
+      const result = await jobsCollection.updateOne (query, update);
+      res.send (result);
     });
 
     app.delete ('/deleteJobs/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
+      const query = {_id: new ObjectId (id)};
       const result = await jobsCollection.deleteOne (query);
       res.send (result);
     });
 
-    app.get('/myAddedJobs', async (req, res) => {
+    app.get ('/myAddedJobs', async (req, res) => {
       const email = req.query.email;
-      const query = { postedBy: email };
-      const jobs = await jobsCollection.find(query).toArray();
-      res.send(jobs);
+      const query = {postedBy: email};
+      const jobs = await jobsCollection.find (query).toArray ();
+      res.send (jobs);
     });
 
-    app.get('/my-accepted-tasks', async (req, res) => {
+    app.get ('/my-accepted-tasks', async (req, res) => {
       const email = req.query.email;
-      const query = { acceptedBy: email };
-      const jobs = await jobsCollection.find(query).toArray();
-      res.send(jobs);
+      const query = {acceptedBy: email};
+      const jobs = await jobsCollection.find (query).toArray ();
+      res.send (jobs);
     });
 
-    app.get('/myAddedJobs', async(req, res) => {
-      const email = req.query.email;
-      const query = { acceptedBy: email };
-      const jobs = await jobsCollection.find(query).toArray();
-      res.send(jobs);
-    })
+    app.patch ('/acceptJob/:id', async (req, res) => {
+      const {id} = req.params;
+      const {acceptedBy} = req.body;
+      const result = await jobsCollection.updateOne (
+        {_id: new ObjectId (id)},
+        {$set: {acceptedBy}}
+      );
+      res.send (result);
+    });
 
+    app.get ('/myAddedJobs', async (req, res) => {
+      const email = req.query.email;
+      const query = {userEmail: email};
+      const jobs = await jobsCollection.find (query).toArray ();
+      res.send (jobs);
+    });
 
     await client.db ('admin').command ({ping: 1});
     console.log (
